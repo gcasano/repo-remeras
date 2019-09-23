8370f72988f60ab9a6f87a698a986f39f1790635
EmanuelTayura <55720128+EmanuelTayura@users.noreply.github.com>
2019-09-23 20:14:07 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<header class="cabeza">
+    <img src="img/descarga.png" class="img-fluid" alt="Responsive image">
+    <nav class="navbar navbar-dark bg-dark">
+      <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="home.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="FQP.php">Preguntas Frecuentes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Registro</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contactos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Mi Perfil</a>
+      </li>
+
+    </ul>
+
+<form class="form-inline">
+<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+</form>
+<!-- Navbar content -->
+</nav>
+
+
+</header>
