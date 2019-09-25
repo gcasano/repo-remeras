7dc8f8adcfa92ee1628a93acd71826c5f06628ac
EmanuelTayura <55720128+EmanuelTayura@users.noreply.github.com>
2019-09-25 11:45:44 -0300

    Add files via upload

---
+++ repo-remeras-master/footer.php
@@ -0,0 +1,13 @@
+
+<footer>
+  <div class="bg-dark p-4">
+    <span class="text-muted">
+      <ul class="navbar">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">F.A.Q</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Perfil</a></li>
+      </ul>
+    </span>
+  </div>
+</footer>
